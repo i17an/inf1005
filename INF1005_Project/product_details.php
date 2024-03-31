5c7398eeef30abf2e40ee7760852f212945cf7fd
Aaronlimzf <163367263+Aaronlimzf@users.noreply.github.com>
2024-04-01 02:35:54 +0800

    Update product_details.php

--- INF1005_Project/product_details.php
+++ INF1005_Project/product_details.php
@@ -3,19 +3,14 @@
 <head>
     <title>Product Details</title>
     <?php include "inc/head.inc.php"; ?>
-    <style>
-        .product-info h2 {
-            margin-bottom: 30px; /* Adjust the value as needed */
-        }
-    </style>
 </head>
-<body>
+<body class="details_page_body">
     <?php include "inc/nav.inc.php"; ?>
     <?php include "inc/header.inc.php"; ?>
     <?php include "functions.php"; ?>
 
-    <main class="container" style="margin-top: 50px;">
-    <section id="product-details" style="margin-bottom: 50px;">
+    <main class="container details_page_main">
+    <section id="product-details">
         <?php
         // Check if the product ID is provided in the query string
         if (isset($_GET['id'])) {
@@ -27,13 +22,15 @@
 
             if ($product) {
                 // Display product details
-                echo '<div class="product-details">';
+                echo '<div class="product-details" style="position: relative;">';
+                // Back button
+                echo '<a href="javascript:history.back()" class="back-button_product-details">&#8592; Back</a>';
                 echo '<div class="product-image">';
                 echo '<img src="' . $product['Image'] . '" alt="' . $product['Name'] . '" class="product-details-img">';
                 echo '</div>';
                 echo '<div class="product-info">';
                 echo '<h2>' . $product['Name'] . '</h2>';
-                echo '<p>Overview:<br>' . $product['Description'] . '</p>';
+                echo '<p><strong>Overview:</strong><br>' . nl2br($product['Description']) . '</p>';
                 echo '<p>Price: $' . number_format($product['Price'], 2) . '</p>';
                 // Add to cart form
                 echo '<form action="add_to_cart.php" method="post">';
@@ -42,6 +39,10 @@
                 echo '<input type="hidden" name="product_price" value="' . $product['Price'] . '">';
                 echo '<button type="submit" class="btn btn-primary">Add to Cart</button>';
                 echo '</form>';
+                // View Cart link
+                echo '<a href="view_cart.php" class="view-cart_product-details">';
+                echo '<i class="fas fa-shopping-cart"></i> View Cart';
+                echo '</a>';
                 // You can add more product details here
                 echo '</div>';
                 echo '</div>';
