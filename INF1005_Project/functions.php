34028c88acb34ee1b03b045f52df26213b8e717c
Aaronlimzf <163367263+Aaronlimzf@users.noreply.github.com>
2024-04-01 19:03:36 +0800

    Update functions.php

--- INF1005_Project/functions.php
+++ INF1005_Project/functions.php
@@ -1,18 +1,7 @@
 <?php
 // Function to retrieve product details from the database based on the product ID
 function getProductDetails($product_id) {
-    // Connect to the MySQL database
-    $servername = "127.0.0.1"; // Change this to your MySQL server
-    $username = "inf1005-sqldev"; // Change this to your MySQL username
-    $password = "password"; // Change this to your MySQL password
-    $database = "Products"; // Change this to your MySQL database name
-
-    $conn = new mysqli($servername, $username, $password, $database);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    include "inc/sql_login.inc.php";
 
     // Prepare SQL query to retrieve product details based on the product ID
     $sql = "SELECT * FROM ProductsTable WHERE ID = $product_id";
