79c6bccd42cc2a12af8d2ee6fc12a5b0528046d6
i17an <149385243+i17an@users.noreply.github.com>
2024-04-01 20:23:16 +0800

    Update register.php

--- INF1005_Project/register.php
+++ INF1005_Project/register.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
     <?php
         include "inc/head.inc.php";
     ?>
+    <style>
+       
+    </style>
+   
 </head>
+
+
 
 
 <body>
@@ -21,8 +28,39 @@
             <div class="text">
             <p>
                 For existing members, please go to the
-                <a href="/login.php">Sign In page</a>.
+                <a href="/login.php">Sign In page</a>
             </p>
+
+
+            <form action = "process_register.php" method = "post">
+                <div class = "mb-3">
+                <label for="fname" class="form-label">First Name:</label>
+                <input type="text" id="fname" name="fname" class="form-control" placeholder="Enter first name">
+                </div>
+
+
+                <div class = "mb-3">
+                <label for="lname" class="form-label">Last Name:</label>
+                <input required maxlength="45" type="text" id="lname" name="lname" class="form-control" placeholder="Enter last name">
+                </div>
+
+
+                <div class = "mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input required maxlength="45" type="email" id="email" name="email" class="form-control" placeholder="Enter email">
+                </div>
+
+
+                <div class = "mb-3">
+                <label for="pwd" class="form-label">Password:</label>
+                <input required type="password" id="pwd" name="pwd" class="form-control" placeholder="Enter password">
+                </div>
+
+                <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Submit</button>
+
+
+            </form>
             </div>
         </section>
     </main>
@@ -32,8 +70,8 @@
     ?>
     <!--End of Footer-->
 
+
     <!--Custom JS-->
     <script defer src="js/main.js"></script>
 </body>
-
 </html>
