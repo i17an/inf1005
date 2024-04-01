61aec415f86e34cb0a6bd990ebb3af01d435fe07
Aaronlimzf <163367263+Aaronlimzf@users.noreply.github.com>
2024-04-01 19:05:13 +0800

    Update thank_you.php

--- INF1005_Project/thank_you.php
+++ INF1005_Project/thank_you.php
@@ -4,10 +4,10 @@
     <title>Thank You</title>
     <?php include "inc/head.inc.php"; ?>
 </head>
-<body>
+<body class="thank_you_page_body">
     <?php include "inc/nav.inc.php"; ?>
     <?php include "inc/header.inc.php"; ?>
-    <div class="container">
+    <div class="container thank_you_page_div">
         <h1>Thank You for Your Order!</h1>
         <p>Your order has been successfully processed. We appreciate your business.</p>
         <!-- You can include additional information, order summary, or a link to the homepage here -->
