f57295a440cf1d415ced3d988cf96a501d91d3c9
i17an <149385243+i17an@users.noreply.github.com>
2024-04-04 02:06:07 +0800

    Create nav.inc.profile.php

---
+++ INF1005_Project/inc/nav.inc.profile.php
@@ -0,0 +1,41 @@
+<nav id="navBar" class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark" aria-expanded="true">
+        <div class="container-fluid">
+            <!--Company's Logo-->
+            <a class="navbar-brand" href="/">
+                <img src="assets/images/logo.png" width="90px" height="90px" alt="logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-tab">
+                        <a class="nav-link" aria-current="page" href="/login_landingPG.php" style="color:#03C988;">HOME</a>
+                    </li>
+                    <li class="nav-tab">
+                        <a class="nav-link" href="/classes.php">CLASSES</a>
+                    </li>
+                    <li class="nav-tab">
+                        <a class="nav-link" href="/shop.php">SHOP</a>
+                    </li>
+                    <li class="nav-tab">
+                        <a class="nav-link" href="/news.profile.php">NEWS</a>
+                    </li>
+                    <li class="nav-tab">
+                        <a class="nav-link" href="/locations.php">FIND A GYM</a>
+                    </li>
+                    <li class="nav-tab">
+                        <a class="nav-link" href="/contact-us.php">CONTACT US</a>
+                    </li>
+                    <li class="nav-item-btn">
+                        <button type="button" class="btn btn-primary"
+                            style="font-size: 21px;background-color: #f00;padding: 0;border-radius:12px;width:150px;font-weight: 600;text-align: center; border: none;">
+                            <a class="nav-link" href="/logout.php">Logout</a>
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
